ced8ea7262e7b16788aca3bbf169bd97253f7fc8
Steven Van Elk <srvanelk@gmail.com>
2025-08-11 14:53:08 -0400

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://weddedpod.com',
+  base: '/',
+  integrations: [mdx(), sitemap()],
+});
